30d5f7508e0a828bab18a8cde8f3395925f639db
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2019-11-07 17:35:59 +0330

    feat(forms): Add iranYekan fontFamily

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './setup/index.css'
 import App from './app/app'
+// styles
+import './setup/index.css'
+import './setup/fonts/fonts.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
